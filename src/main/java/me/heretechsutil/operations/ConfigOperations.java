c0fda10d34415681bc2d6c82a14bf0327eb41179
dixonw2 <mrbacon2015@gmail.com>
2021-12-21 12:19:59 -0500

    Add comments in Operations files for clarity and maintainability

--- src/main/java/me/heretechsutil/operations/ConfigOperations.java
+++ src/main/java/me/heretechsutil/operations/ConfigOperations.java
@@ -9,8 +9,9 @@ import java.util.*;
 public class ConfigOperations {
     private static final HashMap<String, ArrayList<BuyableEntity>> buyables = new HashMap<>();
 
+    // given a category and an item, will return the BuyableEntity associated with it; otherwise null
     public static BuyableEntity getBuyable(String category, String item) {
-
+        // adds an exception for life because Material.matchMaterial would always return null otherwise
         if (item.equalsIgnoreCase("life")) {
             return buyables.get("Miscellaneous").stream().filter(x -> x.getItem().equalsIgnoreCase("life")).findFirst().orElse(null);
         }
@@ -26,6 +27,8 @@ public class ConfigOperations {
         return buyables.get(category.toUpperCase());
     }
 
+    // load buyables from config.yml on startup
+    // item must be a valid Minecraft item tag
     public static void loadBuyables() {
         String methodTrace = "ConfigOperations.loadBuyables():";
         HashSet<String> categories = (HashSet<String>) HeretechsUtil.getInstance().getConfig().getConfigurationSection("buyables").getKeys(false);
@@ -39,6 +42,7 @@ public class ConfigOperations {
         HashMap<String, Object> values = (HashMap<String, Object>) Objects.requireNonNull(HeretechsUtil.getInstance().getConfig().
             getConfigurationSection("buyables." + category)).getValues(false);
 
+        // creates category if it does not exist, and then adds a new BuyableEntity to the category
         values.forEach((item, cost) -> {
             if (!buyables.containsKey(category.toUpperCase())) {
                 buyables.put(category.toUpperCase(), new ArrayList<>());
